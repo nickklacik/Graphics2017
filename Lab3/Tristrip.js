7c40c8e08608646dcad8e173dd2afe26ef9dce2d
nickklacik <nickklacik@gmail.com>
2017-10-02 16:48:47 -0400

    finished lab activity #3

--- Lab3/Tristrip.js
+++ Lab3/Tristrip.js
@@ -1,37 +1,29 @@
-var canvas; // global to hold reference to an HTML5 canvas
-var gl; // global to hold reference to out WebGL contextual
-
-var vetices; // used to store any objects that need to be drawn
-
-var program; // global to hold compiled shader programs
-var vposLoc; // global to hold reference to vertex attributes
-var vBufferId; // global to hold refernce to vertex buffer objects
-
 /* Initialize global WebGL stuff - not object specific */
 function initGL() {
-	// look up out canvas element
-	canvas = document.getElementById("gl-canvas");
-	
+	// local vaiable for HTML5 canvas reference
+	var canvas = document.getElementById("gl-canvas");
 	// obtain a WebGL context bound to our canvas
-	gl = WebGLUtils.setupWebGL(canvas);
-	if (!gl) { alert("WebGL isn't available"); }
-	
-	initShaderProgram();
-	initData();
-	
+	var gl = WebGLUtils.setupWebGL(canvas);
+	if (!gl) { alert("WebGL isn't available"); }	
 	gl.viewport(0, 0, canvas.width, canvas.height); // use the whole canvas
 	gl.clearColor( 0.0, 0.0, 0.0, 1.0); // background color
+	
+//	projectionMatrix = ortho(l,r,b,t,f,n);
+//	projectionMatrix = mult(projectionMatrix, rotate(30, vec(0,1,0)));
+	
+	return gl; // send this back so other parts of the program can use it
 }
 
-/* Initialize the data for a triangle strip. */
-function initData(){
-	vertices = mkstrip(); // this array will hold raw vertex positions
+/* Contructor for a triangle strip object (initializes the data) */
+function TriStrip(gl, program){
+	this.gl = gl; // save my graphics content
+	this.program = program; // save my shader programs
+	this.vertices = mkstrip(); // this array will hold raw vertex positions
+	this.vBufferId = this.gl.createBuffer(); //store a reference to a new buffer object
 	
-	vBufferId = gl.createBuffer(); //reserve a buffer object
-	gl.bindBuffer( gl.ARRAY_BUFFER, vBufferId); // set active array buffer
-	
+	this.gl.bindBuffer( this.gl.ARRAY_BUFFER, this.vBufferId); // set active array buffer
 	// pass data to the graphics hardware (convert JS Array to a typed array)
-	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+	this.gl.bufferData( this.gl.ARRAY_BUFFER, flatten(this.vertices), this.gl.STATIC_DRAW);
 }
 
 /* Build a triangle strip with random heights. */
@@ -58,36 +50,60 @@ function mkstrip(){
 }
 
 /* Load shaders and initialize attribute pointers */
-function initShaderProgram(){
+function loadShaderProgram(gl){
 	// use existing program if given, otherwise use our own defaultStatus
-	program = initShaders( gl, "vertex-shader", "fragment-shader");
-	
+	var program = initShaders( gl, "vertex-shader", "fragment-shader");
 	// get the position attribute and save it to our program
 	// 		then enable the vertex attribute array
-	vposLoc = gl.getAttribLocation( program, "vPosition" );
-	gl.enableVertexAttribArray(vposLoc);
+	program.vposLoc = gl.getAttribLocation( program, "vPosition" );
+	gl.enableVertexAttribArray(program.vposLoc);
+	
+//	program.projLoc = getUniformLocation(program, projMat);
+	
+	return program; // send this back so other parts of the program can use it
+}
+
+/* Method allows an object to render itself */
+TriStrip.prototype.draw = function(gl){
+	gl.useProgram(this.program); // set the current shader programs
+	
+	gl.bindBuffer( gl.ARRAY_BUFFER, this.vBufferId); // set the buffer active
+	// map position buffer data to the corresponding vertex shader attribute
+	gl.vertexAttribPointer(this.program.vposLoc, 3, gl.FLOAT, false, 0, 0);
+	
+	// render the primitives!
+	gl.drawArrays( gl.TRIANGLE_STRIP, 0, this.vertices.length);
+	
+//	gl.unifromMatrix4fv(this.program.projLoc, gl.FALSE, flatten(projectionMatrix));
 }
 
 /* Global render callback to draw all objects */
-function renderScene(){
+function renderToContext(drawables, gl){
+	// inner-scoped funtion for closure trickery
+	function renderScene(){
+		renderToContext(drawables, gl);
+	}
+	
 	// start from a clean frame buffer for this frame
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	
-	gl.useProgram(program); // set the shader programs
-	
-	gl.bindBuffer( gl.ARRAY_BUFFER, vBufferId); // set pos buffer active
-	// map position buffer data to the corresponding vertex shader attribute
-	gl.vertexAttribPointer( program.vposLoc, 3, gl.FLOAT, false, 0, 0);
-	
-	// render the primitives!
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length);
+	drawables.forEach(function(obj){
+		obj.draw(gl);
+	});
 	
 	// queue up this same callback for the next frame
 	requestAnimFrame(renderScene);
 }
 
 /* Set up event callback to start the application */
-window.onload = function() {
-	initGL(); // basic WebGL setup for the scene
-	renderScene(); // start drawing the scene
+window.onload = function(){
+	// local variable to hold reference to our WebGL tontext
+	var gl = initGL(); // basic WebGL setup for the scene
+	var prog = loadShaderProgram(gl);
+	
+	var drawables = []; // used to store a list of objects that need to be drawn
+	
+	drawables.push( new TriStrip(gl, prog)); //create an object and add it to the list
+	
+	renderToContext(drawables, gl);// start drawing the scene
 }
